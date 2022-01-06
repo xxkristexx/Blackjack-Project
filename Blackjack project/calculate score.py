d79fb9f99f02a47b61f70fe3c3e4a93846b774ba
Kristina Pileckaite <kristina.pileckaite@gmail.com>
2022-01-06 12:16:33 +0200

    Initial code

---
+++ Blackjack project/calculate score.py
@@ -0,0 +1,7 @@
+def calculate_score(cards):
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
---
+++ Blackjack project/compare score.py
@@ -0,0 +1,20 @@
+def compare(user_score, computer_score):
+  #Bug fix. If you and the computer are both over, you lose.
+  if user_score > 21 and computer_score > 21:
+    return "You went over. You lose"
+
+
+  if user_score == computer_score:
+    return "Draw"
+  elif computer_score == 0:
+    return "Lose, opponent has Blackjack"
+  elif user_score == 0:
+    return "Win with a Blackjack"
+  elif user_score > 21:
+    return "You went over. You lose"
+  elif computer_score > 21:
+    return "Opponent went over. You win"
+  elif user_score > computer_score:
+    return "You win"
+  else:
+    return "You lose"
